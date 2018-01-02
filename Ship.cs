d140bd6c6d086790ff02e944c774546a29e29926
Dylan <dquesada186@gmail.com>
2018-01-02 14:09:42 -0600

    Added Ship Class

---
+++ Ship.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Ship
+    {
+        // member variables
+        bool sunk = false;
+        int hits = 0;
+
+        // constructor
+        public Ship()
+        {
+            string name;
+            int size;
+        }
+
+        // member methods
+        public int TakeHit()
+        {
+            return hits += 1;
+        }
+        public void SinkShip()
+        {
+            sunk = true;
+        }
+    }
+}
