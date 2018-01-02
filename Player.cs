b4bf87b4a60869c2625ad72138c470575c0bb38b
Dylan <dquesada186@gmail.com>
2018-01-02 10:28:55 -0600

    Added GetUserInput method

--- Player.cs
+++ Player.cs
@@ -9,6 +9,8 @@ namespace Battleship
     class Player
     {
         // member variables
+        public List<string> ships = new List<string>() {  "Destroyer (2)", "Submarine (3)", "Battleship (4)", "Aircraft Carrier (5)" };
+        public List<string> columns = new List<string>();
 
         // constructor
         public Player()
@@ -16,7 +18,17 @@ namespace Battleship
             
         }
         // member methods 
-
+        public string GetUserInput()
+        {
+            return Console.ReadLine();
+        }
+        public void SetBoard()
+        {
+            string selection;
+            for (int i = 0; i < ships.Count(); i++)
+            Console.WriteLine("Where would you like to place your{0}", ships[i]);
+            selection = GetUserInput();
+        }
         
     }
 }
