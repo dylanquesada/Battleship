e19d0259fb15fef2bb24555345eef5c0b495996a
Dylan <dquesada186@gmail.com>
2018-01-03 07:38:30 -0600

    Added PlayerTurn method to Game Class.

--- Game.cs
+++ Game.cs
@@ -23,11 +23,29 @@ namespace Battleship
             Console.WriteLine("Enter player two's name:");
             playerTwo.name = GetUserInput();
             playerTwo.PopulateBoard();
+            while (playerOne.hitsTaken < 14 && playerTwo.hitsTaken < 14)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
             playerTwo.DisplayBoard(playerTwo.columns);
             
         }
 
         // member methods 
+        public void PlayerTurn(Player attackingPlayer, Player defendingPlayer)
+        {
+            string position;
+            Console.WriteLine(" - [- Enemy's Board -] - ");
+            defendingPlayer.DisplayBoard(defendingPlayer.columns);
+            Console.WriteLine("{0}'s Turn.", attackingPlayer.name);
+            Console.WriteLine("Enter your targeting coordinates:");
+            position = GetUserInput();            
+            defendingPlayer.columns = defendingPlayer.FireOnSquare(defendingPlayer.columns);
+           // Console.Clear();
+        }
         public string GetUserInput()
         {
             return Console.ReadLine();
